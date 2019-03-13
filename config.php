47fe3fa14323cda3d1a15b0484436f809feb49c5
DickySyu <47904116+DickySyu@users.noreply.github.com>
2019-03-13 19:40:15 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,46 @@
+<?php
+// urutkan config nya kaya contoh dibawah akun ke 2/3 jg sama !!
+
+// config
+$config = [
+
+  "imei" => [
+// akun ke 1
+"868528027581611",
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "sign" => [
+// akun ke 1
+"34f9525bd50d459a673ed90c97a1b33c975a2209", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "token" => [
+// akun ke 1
+"2119d81deb29c14a9fa0675f5b036c2f", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "uuid" => [
+// akun ke 1
+"c3acb5361a114029", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+
+  ];
+
+?>
